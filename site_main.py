190b88b233fb5df65a2dab9235a62b12e53fac11
Dzmitry Sakalenka <blackvehkleja@gmail.com>
2020-10-26 16:46:45 +0300

    upload file

--- site_main.py
+++ site_main.py
@@ -1,18 +1,36 @@
 from flask import *  
+import os
 
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)  
- 
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 @app.route('/')  
 def upload():  
     return render_template("index.html")  
- 
+
+
+# sudoku_puzzle.jpg
 @app.route('/success', methods = ['POST'])  
 def success():  
     if request.method == 'POST':  
-        f = request.files['file']  
-        f.save(f.filename)  
-        return render_template("success.html", name = f.filename)  
-  
+        f = request.files['file']
+        print(f.filename)
+        print(type(f))
+        if f and allowed_file(f.filename):
+            f.filename = 'sudoku_puzzle.' + f.filename.rsplit('.', 1)[1].lower()
+            f.save('static/' + f.filename)
+            return render_template("success.html", name = f.filename)  
+        else:
+            return render_template("index.html") 
+
+
 if __name__ == '__main__':  
     app.run(debug = True)  
